dac40c3bde45238041fba63a989b9d30cbd7770e
husain <husain68644@gmail.com>
2024-06-21 10:20:40 +0530

    first-commit

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 git init
 git add .
 git commit -m "first-commit"
-git add origin url git@github.com:Husain644/React-Native-Comp.git
+git remote set-url origin git@github.com:Husain644/React-Native-Comp.git
