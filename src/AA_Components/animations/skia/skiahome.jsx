7751ae2d5cfac55efff6232a23d8825c0888f75b
husain <husain68644@gmail.com>
2025-06-29 11:04:59 +0530

    changes

--- src/AA_Components/animations/skia/skiahome.jsx
+++ src/AA_Components/animations/skia/skiahome.jsx
@@ -1,37 +1,101 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
-import React,{useState} from 'react'
-import { usePathValue,Canvas,Skia,Path,vec } from '@shopify/react-native-skia'
-import Animated,{useSharedValue, withTiming} from 'react-native-reanimated'
-import { Gesture,GestureDetector } from 'react-native-gesture-handler'
-
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { wordA } from "./data/data";
+import {
+  usePathValue,
+  Canvas,
+  Skia,
+  Path,
+  vec,
+} from "@shopify/react-native-skia";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 const Skiahome = () => {
- const [path,setPath]=useState(Skia.Path.Make())
- const newPath=usePathValue((p)=>{ 'worklet'},path)
-  const panGesture=Gesture.Pan().onBegin((e)=>{
-      path.moveTo(e.x,e.y)
-  }).onUpdate((e)=>{
-    path.lineTo(e.x,e.y) 
-  })  
-  return (
-    <View>  
-      <Text>Skiahome</Text>  
-        <GestureDetector gesture={panGesture}>
-      <Canvas style={{backgroundColor:'#ccc',height:550}}>
-        <Path path={newPath}  color="green" style="stroke" strokeWidth={10} /> 
-      </Canvas>
+  console.log('update')
+  const Pos = useSharedValue({ x: wordA[0].x, y:wordA[0].y });
+  const [path, setPath] = useState(Skia.Path.Make());
+  const path2 = Skia.Path.Make();
+  const newPath = usePathValue((p) => {
+    "worklet";
+  }, path);
+  const panGesture = Gesture.Pan()
+    .onBegin((e) => {
+      if ((e.x >wordA[0].x) & (e.x <wordA[1].x) & (e.y > wordA[0].y) & (e.y < wordA[1].y)) {
+        path.moveTo(e.x, e.y);
+      } else {
+        console.log('origin',wordA[0].x,wordA[0].y);
+        path.moveTo(wordA[0].x,wordA[0].y);
+      }
+      Pos.value = {
+        x: e.x,
+        y: e.y
+      };
+    })
+    .onUpdate((e) => {
+      Pos.value = {
+        x: e.x,
+        y: e.y,
+      };
+      if ((e.x >wordA[0].x) & (e.x <wordA[1].x) & (e.y > wordA[0].y) & (e.y < wordA[1].y)) {
+        path.lineTo(e.x, e.y);
+      }
+    });
+  const AnimStyle = useAnimatedStyle(() => {
+    return {
+      left: Pos.value.x - 10,
+      top: Pos.value.y + 8,
+    };
+  });
+  
+  wordA.forEach((Item,index)=>{
+    if (index===0){
+      path2.moveTo(Item.x,Item.y);
+      console.log('okkk')
+    }else{
+     path2.lineTo(Item.x,Item.y);
+    }
+  })
+
+return (
+    <View style={{}}>
+      <Text>Skiahome</Text>
+      <GestureDetector gesture={panGesture}>
+        <Canvas style={{ backgroundColor: "#ccc", height: 650 }}>
+          <Path path={newPath} color="green" style="stroke" strokeWidth={10} />
+          <Path path={path2} color="white" style="stroke" strokeWidth={2} />
+        </Canvas>
       </GestureDetector>
-       <Animated.View  style={{width:20,height:20,backgroundColor:'red',
-        left:20,
-        borderRadius:20}}>
-        </Animated.View>
-        <TouchableOpacity  onPress={()=>{setPath(Skia.Path.Make())}} style={{width:200,backgroundColor:'#eaea'}}>
-          <Text style={{fontSize:30,textAlign:'center'}}>refresh</Text>
-        </TouchableOpacity>
+      <Animated.View
+        style={[
+          {
+            width: 20,
+            height: 20,
+            backgroundColor: "red",
+            position: "absolute",
+            left: 0,
+            top: 0,
+            borderRadius: 20,
+          },
+          AnimStyle,
+        ]}
+      ></Animated.View>
+      <TouchableOpacity
+        onPress={() => {
+          setPath(Skia.Path.Make());
+        }}
+        style={{ width: "100%", backgroundColor: "#eaea", borderRadius: 10 }}
+      >
+        <Text style={{ fontSize: 30, textAlign: "center" }}>refresh</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 export default Skiahome;
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
